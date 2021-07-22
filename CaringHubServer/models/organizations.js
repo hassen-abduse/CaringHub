a728008658dce983715f227fd59a6a808f63822f
hassen-abduse <hassen.abduse@gmail.com>
2021-07-22 22:13:03 +0300

    added the organization schema

---
+++ CaringHubServer/models/organizations.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
+var Schema = mongoose.Schema;
+
+var Organization = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    address: {
+        type: Map,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        unique: true,
+        default: ''
+    },
+    emailAddress: {
+        type: String,
+        unique: true,
+        default: ''
+    },
+    adminName: {
+        type: String,
+        required: true,
+    },
+    adminPosition: {
+        type: String,
+        required: true
+    },
+    mission: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+Organization.plugin(passportLocalMongoose);
+module.exports = mongoose.model('Organization', Organization);
