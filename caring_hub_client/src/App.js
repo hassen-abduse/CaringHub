0bf0ab94450ab7aa867341c44576bf506cc9a073
Mehammed <checoslbches@gmail.com>
2021-07-27 11:01:10 -0900

    update on landing page

--- caring_hub_client/src/App.js
+++ caring_hub_client/src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
+import React from "react";
+//material ui imports
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
+import Landing from "./Landing/Landing";
 //import SignIn from "./Roles/common/SignIn";
 import SignUp from "./Roles/common/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Landing maxWidth="sm">
+        <Typography
+          component="div"
+          style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
+        />
+      </Landing>
+    </React.Fragment>
   );
 }
 
