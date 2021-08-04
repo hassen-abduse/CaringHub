7af9fb1683757b80374826d2ad18af399914b940
hassen-abduse <hassen.abduse@gmail.com>
2021-08-04 20:41:38 +0300

    added /login route

--- CaringHubServer/app.js
+++ CaringHubServer/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var config = require('./config');
 var mongoose = require('mongoose');
+var passport = require('passport');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,7 +18,7 @@ connect.then((db) => {
   console.log('Succesfully Connected to the DB Server.');
 }, (err) => console.log(err));
 
-
+require("./routes/auth");
 var app = express();
 
 // view engine setup
@@ -26,19 +28,38 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(passport.initialize());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.post('/login', (req, res, next) => {
+  passport.authenticate(['user-local', 'vol-local', 'org-local'], (err, user, info) => {
+    var error = err || info;
+    if (error) return res.json(401, error);
+    req.logIn(user, (err) => {
+      if (err) return res.send(err);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({ success: true, status: 'Login Success!' });
+    });
+  })(req, res, next);
+});
+
+app.get('/logout', (req, res, next) => {
+  
+});
+
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
