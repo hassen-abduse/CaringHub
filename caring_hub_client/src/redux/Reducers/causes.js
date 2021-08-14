cf847fc5f84a1f751d2f2345fe3e185601f3efff
hassen-abduse <hassen.abduse@gmail.com>
2021-08-14 17:55:23 +0300

    added causes reducer

---
+++ caring_hub_client/src/redux/Reducers/causes.js
@@ -0,0 +1,38 @@
+import * as actionTypes from '../actionTypes'
+
+export const Causes = (state = {
+  isLoading: true,
+  errMess: null,
+  causes: []
+}, action) => {
+  switch (action.type) {
+    case actionTypes.CAUSES_LOADING:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: null,
+        causes: action.payload
+      }
+    case actionTypes.ADD_CAUSES:
+      return {
+        ...state,
+        isLoading: true,
+        errMess: null,
+        causes: []
+      }
+    case actionTypes.CAUSES_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: action.payload,
+        causes: []
+      }
+    case actionTypes.ADD_CAUSE_ITEM:
+      return {
+        ...state,
+        causes: state.causes.concat(action.payload)
+      }
+    default:
+      return state
+  }
+}
