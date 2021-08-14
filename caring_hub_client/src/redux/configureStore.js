9b60c1d25f6239926b3e3b22f366758d827ca6b1
hassen-abduse <hassen.abduse@gmail.com>
2021-08-14 18:28:57 +0300

    =configured store with reducers

---
+++ caring_hub_client/src/redux/configureStore.js
@@ -0,0 +1,31 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import { Auth } from './Reducers/auth'
+import { Projects } from './Reducers/projects'
+import { Volunteers } from './Reducers/volunteers'
+import { Organizations } from './Reducers/organizations'
+import { Causes } from './Reducers/causes'
+import { HelpRequests } from './Reducers/helpRequests'
+import { Applications } from './Reducers/applications'
+import { Users } from './Reducers/users'
+import { Skills } from './Reducers/skills'
+
+export const __store__ = () => {
+  const store = createStore(
+    combineReducers({
+      auth: Auth,
+      Projects,
+      Volunteers,
+      Organizations,
+      Applications,
+      HelpRequests,
+      Users,
+      Skills,
+      Causes
+    }),
+    // to log state and changes to state by actions
+    applyMiddleware(thunk, logger)
+  )
+  return store
+}
