1769afe89f8d6d4562f0818fcb94b532db45f327
hassen-abduse <hassen.abduse@gmail.com>
2021-08-14 17:57:41 +0300

    added helpRequests reducer

---
+++ caring_hub_client/src/redux/Reducers/helpRequests.js
@@ -0,0 +1,38 @@
+import * as actionTypes from '../actionTypes'
+
+export const HelpRequests = (state = {
+  isLoading: true,
+  errMess: null,
+  helpRequests: []
+}, action) => {
+  switch (action.type) {
+    case actionTypes.HELPS_LOADING:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: null,
+        helpRequests: action.payload
+      }
+    case actionTypes.ADD_HELPS:
+      return {
+        ...state,
+        isLoading: true,
+        errMess: null,
+        helpRequests: []
+      }
+    case actionTypes.HELPS_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: action.payload,
+        helpRequests: []
+      }
+    case actionTypes.ADD_HELP_ITEM:
+      return {
+        ...state,
+        helpRequests: state.helpRequests.concat(action.payload)
+      }
+    default:
+      return state
+  }
+}
