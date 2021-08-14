3c6924b6f1c31cf1a5e1fdd689e33c5953093ecb
hassen-abduse <hassen.abduse@gmail.com>
2021-08-14 17:54:57 +0300

    add skills reducer

---
+++ caring_hub_client/src/redux/Reducers/skills.js
@@ -0,0 +1,38 @@
+import * as actionTypes from '../actionTypes'
+
+export const Skills = (state = {
+  isLoading: true,
+  errMess: null,
+  skills: []
+}, action) => {
+  switch (action.type) {
+    case actionTypes.SKILLS_LOADING:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: null,
+        skills: action.payload
+      }
+    case actionTypes.ADD_SKILLS:
+      return {
+        ...state,
+        isLoading: true,
+        errMess: null,
+        skills: []
+      }
+    case actionTypes.SKILLS_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: action.payload,
+        skills: []
+      }
+    case actionTypes.ADD_SKILL_ITEM:
+      return {
+        ...state,
+        skills: state.skills.concat(action.payload)
+      }
+    default:
+      return state
+  }
+}
