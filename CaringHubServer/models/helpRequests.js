e6a665ea47ee82f90f7bb5022efd258ed9c0d836
hassen-abduse <hassen.abduse@gmail.com>
2021-08-23 21:23:59 +0300

    updated data models

--- CaringHubServer/models/helpRequests.js
+++ CaringHubServer/models/helpRequests.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const helpRequestSchema = new Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    resolved: {
-        type: Boolean,
-        default: false
-    },
-    applicationDate: {
-        type: Date,
-        default: Date.now()
-    },
-    location: {
-        type: Map,
-        required: true
-    }
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  resolved: {
+    type: Boolean,
+    default: false
+  },
+  applicationDate: {
+    type: Date,
+    default: Date.now()
+  },
+  location: {
+    type: Map,
+    required: true
+  }
 }, {
-    timestamps: true
-});
+  timestamps: true
+})
 
-var HelpRequests = mongoose.model('HelpRequest', helpRequestSchema);
-module.exports = HelpRequests;
+const HelpRequests = mongoose.model('HelpRequest', helpRequestSchema)
+module.exports = HelpRequests
--- CaringHubServer/models/projects.js
+++ CaringHubServer/models/projects.js
@@ -1,39 +1,39 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const projectSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    startDate: {
-        type: Date,
-        required: true
-    },
-    endDate: {
-        type: Date,
-        required: true
-    },
-    ownerOrg: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Organization'
-    },
-    location: {
-        type: Map,
-        required: true
-    },
-    skillSets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Skill' }],
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  startDate: {
+    type: Date,
+    required: true
+  },
+  endDate: {
+    type: Date,
+    required: true
+  },
+  ownerOrg: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Organization'
+  },
+  location: {
+    type: Map,
+    required: true
+  },
+  skillSets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Skill' }],
 
-    causeAreas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Cause' }]
+  causeAreas: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Cause' }]
 
 }, {
-    timestamps: true
-});
+  timestamps: true
+})
 
-var Projects = mongoose.model('Project', projectSchema);
-module.exports = Projects;
+const Projects = mongoose.model('Project', projectSchema)
+module.exports = Projects
