e12ec52c9801c3f650deba02c0cb52f1c209f0f2
hassen-abduse <hassen.abduse@gmail.com>
2021-08-23 21:34:07 +0300

    added users action creators

---
+++ caring_hub_client/src/redux/ActionCreators/userActions.js
@@ -0,0 +1,37 @@
+import * as actionTypes from '../actionTypes'
+import { baseUrl } from '../shared/baseUrl'
+
+export const usersLoading = () => ({
+    type: actionTypes.USERS_LOADING
+});
+
+export const usersFailed = (errmess) => ({
+    type: actionTypes.USERS_FAILED,
+    payload: errmess
+});
+
+export const addUsers = (users) => ({
+    type: actionTypes.ADD_USERS,
+    payload: users
+});
+
+export const fetchUsers = () => (dispatch) => {
+    dispatch(usersLoading(true))
+    return fetch(baseUrl + 'users')
+        .then(response => {
+            if (response.ok) {
+                return response
+            } else {
+                const error = new Error('Error ' + response.status + ': ' + response.statusText)
+                error.response = response
+                throw error
+            }
+        },
+            error => {
+                const errMess = new Error(error.message)
+                throw errMess
+            })
+        .then(response => response.json())
+        .then(users => dispatch(addUsers(users)))
+        .catch(error => dispatch(usersFailed(error.message)))
+}
