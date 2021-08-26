dcb69d9956e7b15fc48e9200d4211f27074156bc
hassen-abduse <hassen.abduse@gmail.com>
2021-08-26 08:58:29 +0300

    added post and delete methods for applications

--- caring_hub_client/src/redux/ActionCreators/appActions.js
+++ caring_hub_client/src/redux/ActionCreators/appActions.js
@@ -40,3 +40,57 @@ export const fetchApplications = () => (dispatch) => {
         .then(apps => dispatch(addApps(apps)))
         .catch(error => dispatch(appsFailed(error.message)))
 }
+
+export const postApplication = (formData) => (dispatch) => {
+    const bearer = 'Bearer ' + localStorage.getItem('token')
+    return fetch(baseUrl + 'applications', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+            'Authorization': bearer
+        }
+
+    })
+        .then(response => {
+            if (response.ok) {
+                return response
+            }
+            else {
+                const error = new Error('Error ' + response.status + ': ' + response.statusText)
+                error.response = response
+                throw error
+            }
+        },
+            error => {
+                const errMess = new Error(error.message)
+                throw errMess
+            })
+        .then(response => response.json())
+        .then(response => dispatch(addAppItem(response)))
+        .catch(error => alert('APPLICATION COULD NOT BE POSTED: ' + error))
+}
+
+export const deleteApplication = (appId) => (dispatch) => {
+    const bearer = 'Bearer ' + localStorage.getItem('token')
+    return fetch(baseUrl + 'applications/' + appId, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': bearer
+        }
+    })
+        .then(response => {
+            if (response.ok) {
+                return response
+            } else {
+                const error = new Error('Error ' + response.status + ': ' + response.statusText)
+                error.response = response
+                throw error
+            }
+        }, error => {
+            throw error
+        })
+        .then(response => response.json())
+        .then(response => dispatch(addApps(response)))
+        .catch(error => dispatch(appsFailed(error)))
+}
+
