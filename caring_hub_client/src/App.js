e047984adeeacbab199e117d31c2e9475b3037d4
AmirMustefa <32706438+AmirMustefa11@users.noreply.github.com>
2021-09-05 18:37:36 +0300

    Applicant page ui improved

--- caring_hub_client/src/App.js
+++ caring_hub_client/src/App.js
@@ -5,6 +5,8 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import Main from "./components/Main";
 import { __store__ } from "./redux/configureStore";
+import Volunteers from "./organization/pages/volunteers/Volunteers";
+import Applicants from "./organization/pages/applicants/Applicant";
 
 const store = __store__();
 class App extends Component {
@@ -13,7 +15,8 @@ class App extends Component {
       <Provider store={store}>
         <BrowserRouter>
           <div>
-            <Main />
+            {/* <Volunteers /> */}
+            <Applicants />
           </div>
         </BrowserRouter>
       </Provider>
